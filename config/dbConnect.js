d076a93b0c55b4e50df624fcc1d23a6449bd9f8e
Phaneesh28 <95366988+Phaneesh28@users.noreply.github.com>
2024-12-05 19:37:31 -0700

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-
+const mongoURI ='mongodb+srv://admin:Admin@123@cluster0.zbool.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+if (!mongoURI) {
+  console.error("MongoDB URI is undefined. Please check your connection string.");
+  process.exit(1); // Exit the application if the URI is not set
+}
 const ConnectDb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:Admin@123@cluster0.zbool.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect('mongodb+srv://admin:Admin@123@cluster0.zbool.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
