3a3213d4016df8c807e3e45fff8c91eb8e7c97b1
Phaneesh28 <95366988+Phaneesh28@users.noreply.github.com>
2024-12-05 19:31:58 -0700

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ConnectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://admin:Admin@123@cluster0.zbool.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
