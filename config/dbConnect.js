1147f4943945af38314c7e5073da5fd952c58c2c
Phaneesh28 <95366988+Phaneesh28@users.noreply.github.com>
2024-12-05 18:24:33 -0700

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const ConnectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected Successfully');
   } catch (err) {
-    console.log(`${err.message}`)
+    console.error('MongoDB Connection Error:', err.message);
+    process.exit(1); // Exit process on failure
   }
-}
-module.exports = ConnectDb
+};
+
+module.exports = ConnectDb;
